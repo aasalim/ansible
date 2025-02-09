e06f5747524d7179c3cac308508b0f0435e2750e
Ahmed Salim <ahmedslim12@gmail.com>
2025-02-09 07:41:24 -0500

    fix

--- roles/neovim/files/lua/custom/file_explorer.lua
+++ roles/neovim/files/lua/custom/file_explorer.lua
@@ -1,15 +1,35 @@
 
 require('oil').setup {
-	columns = {
-		"icon",
-		"permissions",
-		"size",
-		"mtime",
-	},
-		float = {
-			open_by_default = true
-		},
-		view_options={ show_hidden=true },
+  columns = {
+    "icon",
+    "permissions",
+    "size",
+    "mtime",
+  },
+  float = {
+    open_by_default = true
+  },
+  view_options={ show_hidden=true },
+  -- Keymaps in oil buffer.
+  keymaps = {
+    ["g?"] = { "actions.show_help", mode = "n" },
+    ["<CR>"] = "actions.select",
+    ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+    ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+    ["<C-t>"] = { "actions.select", opts = { tab = true } },
+    ["<C-p>"] = "actions.preview",
+    ["<C-c>"] = { "actions.close", mode = "n" },
+    ["<C-l>"] = "actions.refresh",
+    ["-"] = { "actions.parent", mode = "n" },
+    ["_"] = { "actions.open_cwd", mode = "n" },
+    ["`"] = { "actions.cd", mode = "n" },
+    ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+    ["gs"] = { "actions.change_sort", mode = "n" },
+    ["gx"] = "actions.open_external",
+    ["g."] = { "actions.toggle_hidden", mode = "n" },
+    ["g\\"] = { "actions.toggle_trash", mode = "n" },
+  },
 }
-vim.keymap.set("n", "<Leader>e",require("oil").toggle_float, { desc = "Open parent directory" })
-        vim.treesitter.language.register('markdown', 'vimwiki')
+
+vim.keymap.set({"n","v"}, "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.treesitter.language.register('markdown', 'vimwiki')
